d256936a8492ddc384ee77c491fab85cde81ccfa
Abhijeet <abhijeetab05@gmail.com>
2026-03-04 11:25:35 +0530

    Added Patient model schema

---
+++ backend/models/Patient.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const patientSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Patient", patientSchema);
